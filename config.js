4e6670226bb5a38971cc1b65c56adf7704011952
wdlock <pupzeriina@outlook.com>
2023-11-29 01:47:32 +0000

    Update 4 files

---
+++ config.js
@@ -0,0 +1,75 @@
+/*
+* https://rentry.org/teralomaniac_clewd
+* https://github.com/teralomaniac/clewd
+*/
+
+// SET YOUR COOKIE BELOW
+
+module.exports = {
+    "Cookie": "SET YOUR COOKIE HERE",
+    "CookieArray": [
+        "__cf_bm=bVrMKUE..zx6BpIK1iJyaJ9F9tZpgmbTb6MRoCn7y.8-1697264594-0-AaHRw+QRUgpFHOUJE6xT3e+OIgqo5gBDMwJATAZQvxRkbo74aRHKDhh2A5o6t3CAgy8B1JND145Tjm7cElKpxDM=; activitySessionId=7859849e-f24b-4922-a54d-7a407d64128e; sessionKey=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; __ssid=c6fd861d964e3c901182629e0214234; intercom-device-id-lupk8zyo=73947767-50a8-42eb-a6f5-ba26cf2cfe61; intercom-session-lupk8zyo=UDB2bEIxczdFKzJiV2lJZ1gzNW1PTUxQUFZiWklFYWpSa0F1N0pLTGQ4ZTdPcnYrOCtodFJoZlNBTTBYNmo4Ly0tUEYxS2lJUkRrL3hNYUZuSkQvK0ozQT09--923bf9174c603842e850100c8238ebb26c1a1ccfsessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=a9afa748-ca81-4f1f-98f1-b0ee6f2cc342; __cf_bm=k7UrngZ3SetvC2CGfJpb_9G8xJrWUQwyJL9AbeyP09Y-1697266018-0-Ae1O4443R3ZQyaUxnsXtEghzRLYEEscrvlZKvIHQ/ViNfInVJ2rm343q2uzYlvjeajyLLqGpKfFoT+qko0sUJ+o=; cf_clearance=P8N_C1n_VHfM__2uybA6e1nXgRC56jpfbyEuMV7pCd0-1697266020-0-1-8df648f6.f4131e4e.daf71716-0.2.1697266020; __ssid=690c44526accfe637ab39d12df64fef; sessionKey=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; intercom-device-id-lupk8zyo=d6c8d931-2761-4c51-801d-d420e898a0f0; intercom-session-lupk8zyo=cUU3UEd3eUIxRWxMMmpxVFJIRnRDeVgxMWZvUmw2WXlwWmV2dWxpcFRWSHlBUitDdkFnckZ2UDJWM3RxZU9XTi0taFZNeTJsckwwMUpiR09KWS9YdklLdz09--84fee0d2ed832e3aaaffb0010c3035181575f824sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=e64882ef-2adf-4699-936b-3faee6a6f7ed; __cf_bm=RKJ9fCwmPMrzZu2SuF9d24s1pclHiY9qVsML8igUK2Y-1697266270-0-AVQ8CMflhiR2vSXCqWZ/eGnTiYD7Vi7LNIDVQ627Ll/odEGTzJKm9uh6vfiUbRoiJXqb7MMATQ8r6iDWGVWUQeE=; __ssid=6b4ec6a8b8e70120535452ef9d9951f; cf_clearance=9Gq_Fg8jWcDZqzqC24MxnF7TLqZoZ0_CdGLpmsrOkfE-1697266280-0-1-8df648f6.5900798e.daf71716-0.2.1697266280; sessionKey=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; intercom-device-id-lupk8zyo=5a450e6c-b885-4a82-a873-97350a92d45a; intercom-session-lupk8zyo=aFJCMENPbEM2czllUitVbVRDNTNIWFRFVUVhdVBIbU5IZStIQml2KzZ1VGZOVjVVT2FTenNhL0lXODdvNU5FYi0tKzBvQmZ2THpmQk5Qcis3NS9FQWoxQT09--641123da34a83891e9ac7b70599529903a1f1873sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=5a8f647b-772a-449a-a132-a8f2381dc692; __cf_bm=CXsQt11zG71EAVOC.wiD1Oc9hae09onWzs_qcrdxMnw-1697266482-0-AXYQDFYE3FvV4ppDHyO/8y37ekMUiwAMktqOZnaM6yxQfGs1MNdedtUHRZB9i6dstwNkbLsu1F5zy6cMfxx4VRM=; cf_clearance=erOVZ6hMC9s64jnsabNBqNiIw6QvGbEhqm0aoGLSc2k-1697266484-0-1-8df648f6.60bf892c.daf71716-0.2.1697266484; __ssid=872e5998231339f3b9aff0c94c3bab3; sessionKey=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; intercom-device-id-lupk8zyo=f103d364-6f41-405e-8ada-e21368a0b961; intercom-session-lupk8zyo=UmJoUWp5WFFhUlptdzlMdmtoWVFnaVF6ZGRyS0VoTGNYelpNSGFwWHUwRjNJbkx4cXRLSWhvSkRRRzgycFhUbC0taFpNckttVTI4b3ljOU15TGp1QVUrZz09--c63bb92d78d11cc93eecfc5203737999c3a78b7csessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=2f374aa6-75bf-4a1d-8593-c3ea7a38999e; __cf_bm=Vfbg7hShfyvSUW_I93WSqgk6EjNUVeFoA0m1MKYPpUk-1697266633-0-AYYFzY9j0G5qeMcxT/2biLZ2J5DcyYfg1EPhcFHHFkXP7P5+OlqOHac9r/aBTFnMjnNwmlzd1jmlIOh6SeFgFaM=; cf_clearance=RcmYE6pQ8BLfMpWoRkhPWcYETaacR8PzxXKRgPt_FFM-1697266637-0-1-8df648f6.e4c08bd9.daf71716-0.2.1697266637; __ssid=c393de6ef0748f53b8669022455a9f9; sessionKey=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; intercom-device-id-lupk8zyo=2690cb49-dc02-45c7-ba77-c083feb0a511; intercom-session-lupk8zyo=OVdLOE5HUlkyRTM3ZmpiWE1nZitsNDFpZFp6UURocTlLd2NsSWlYcExXNjE1N1ZscVd4NDRLM0U5dHNtMnRJaS0tZndzN0RKNjIyek9pbFdYMlpPTHJrQT09--cfc02672f136afe09b21fbfd5755eb870591e121sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=0c34198e-4639-4b3b-aec5-73543d7247c9; __cf_bm=6LC58WFfy6U1Gp0WuJaEpT8DpAVcmg4SfKCon_IptG0-1697287024-0-AepmTJ9lWSM1bxZjihOWzKZb4aMJx/QlLxXmlITH7OGsXb4FGYMFba2u1COPlHlSDcnCtgu1ttdgHmDuScUPU7c=; __ssid=118f122d001193c2d12a42db4399a1a; cf_clearance=XUB5npWKY4FArWmweqC6vhXX6E5ZJZ7uH0OpPUqi9qg-1697287045-0-1-8df648f6.93b4e9a6.daf71716-0.2.1697287045; sessionKey=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; intercom-device-id-lupk8zyo=e860ca45-0f0d-49d8-841c-75b4e2628af5; intercom-session-lupk8zyo=MlhkdFpPWVFPV2RvbG9HVDUxUGl1RldXNkFCZVhsbEQzdk5xUnF5d21jdDFoUnV6QkhzcG5KWldPKzUzVlJxYi0tbEMrZFJNSGw2bnlVQW9KYlp4bGVmQT09--00a8c8ec1f1d71b9541b14a40a85151335ae8729sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=b7063a8a-f04d-4d4b-93b6-afea5336d7b7; __ssid=b8da0b849edb1e032cbb891c8cd43b5; __cf_bm=9UqKawvIRww8JhXjEJPbQ5mvdVljo_hJO4BC1UpfjjE-1697348679-0-AT+YPSXz1p09SwkyNyjqlUvNIla4XfE8o1PShpR336DKNiUEVYEgn9LTT6eW39glwVdy69icRtbqfbEsT1ZNufg=; cf_clearance=viXhMBrMOvPRBHgBXu5zkhuhWZydjRVWKevsITNS6QU-1697348680-0-1-8df648f6.280d8b4c.daf71716-0.2.1697348680; sessionKey=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; intercom-device-id-lupk8zyo=58b2c8ad-ce61-4525-8be7-e3e382cb374a; intercom-session-lupk8zyo=NHYyTTFMMTNLdnd0Ykxmb3YrOFVRVGdvZGZ5d2UwajA4U3RJT2dHbTZHTHJsRXBsQXpTVmE5OUtLQ2Q3RXpvRi0tZDBLckpzeXpYSXQxVERGK1JlRzBodz09--8cafa6ed6f2840d4322bdb64da116c0b01be863csessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=17c3b920-f9ee-43fa-9c25-de97d86bcc41; __cf_bm=60aA8eY768qq6IzYmbmxZJPchZxCG56eyfEFS6DaUy8-1697513719-0-AaK7URRuTHAkTNNLTUJDrLfQCGF1acT9Zgsna9pnLvl+jznyroZI0l3wqcZ4kN8vI7Ey0Jl84OoLA87B/D3XEJE=; cf_clearance=tuTi4d4KmydSbElCb6QV1C0E0u2Ps1NzUnj_CD9MZJM-1697513720-0-1-8df648f6.5a87f73e.daf71716-0.2.1697513720; __ssid=2ddbba3f016241509dd1429ca4ed0a3; sessionKey=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; intercom-device-id-lupk8zyo=752fcfd1-c6e7-473f-a8db-4a95bd1e804a; intercom-session-lupk8zyo=VHVQZktNcTFVVSt4RlJUcmlEb1pYTXlxZytLc0RlMFVPblZRVGN3SmIyamFlM0JtSytrZWZqZldhQjBSMGRKQi0tSGJwOGRTbUF0c2FpVTNxM3ErZlJTUT09--a8200ead1320bf9ce29ad2f98191e18266025b80sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=fa9b71c2-0d6e-47b2-9b8b-0edb0feca469; __cf_bm=W2HoDzCM8FGUEX_h9AP_JjB6dErR60UqMpl86_pgsPQ-1697532478-0-AT5J/v6hSilPI8oELjNQwianWZxSymDazZ2WGeCa6Gcb6xlIEvl7ZVFxj1pcysim2cV+xyMPJsyLlFSLS2kixQg=; cf_clearance=c48rUka_sg1aEhCOz6CblqYRiEyuq1Vk5LeCurtHmrQ-1697532479-0-1-8df648f6.ed09f9b5.daf71716-0.2.1697532479; __ssid=cbf291d0d73310535b94a9b5f9ca563; sessionKey=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; intercom-device-id-lupk8zyo=39787872-9e20-4231-95cb-d50348ac2736; intercom-session-lupk8zyo=NUZKWlpZOWJJVHBjdmh0OUc4ZUMwSzMwK0piMGRoRDFMZnRFOUxKS1dPc3h4R1JFMjZPektvU0lMTmlaYjJLQS0tRGZ6d2gyQ3RsdTdKN2c1Z0k5SmJUQT09--20a5127092271bba5bab46278eac1c78b4405323sessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=1bbe2b6e-eff8-4387-b329-587fa27799aa; __cf_bm=z1zECShgI.jnezN1AXfWs2yXCybp8A57YKqtcpilB0g-1697860993-0-Aa1QsJwR3DW++6aLMipb4WaRb9s6/S2shptw53QV8Sd9N5RJbUzT7N0J1AR+5seb+n/2AWCYEPhgjOoTITxqJOM=; cf_clearance=zNZqUBdGqdEejcJPERUpciFtP1W7_ZDc2Ge5NUunQC4-1697860994-0-1-9ac1b33.3dd9c8a5.5ead44d3-0.2.1697860994; __ssid=7896797153a383604e1941332a1ce1e; sessionKey=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; intercom-device-id-lupk8zyo=3361b9dd-efbf-4c23-866a-06e6ad02feb3; intercom-session-lupk8zyo=Sk16ZXVRekkyM3hmT29PcVhPNWJkVWFZNjc4Wm1icTRLckdCNUZTZ1JLcXZTcDJVK3k5ZVVVV0kxOUpKb1hvTy0tT2JES1pxbkZFcHROdjZQU2NySEsxZz09--866e92876fb8454772316666a5dbf513738375fcsessionKey=sessionKey=sessionKey=sessionKey=",
+        "activitySessionId=ef4c4a0a-a951-4715-843f-c7cf1a744680; __cf_bm=AA_WpHLx0vwBye26spUhjQvHNCRNeeNjSox_imspSXg-1697861597-0-AbJtqoQMNYGtl3ZVJzM+L9zAIhT+ocYPGRpcuiCTrGJrW9K41hoqmJbsGzgmeOO1XtTi3dcgJpBQCj04XmL3YyA=; cf_clearance=vByRoKMdGZ29O2tlTqtx0rdH_RVLeIKQFVByTvOGzzc-1697861598-0-1-9ac1b33.f080db7d.5ead44d3-0.2.1697861598; __ssid=99e49a76a8457e6318322926cc1406a; sessionKey=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; intercom-device-id-lupk8zyo=47602ca1-780e-415d-a5c9-aa93e31e9f66; intercom-session-lupk8zyo=dTZoWGtpT3JabGlqQnQ5NkFvRkZWVlVsMlRnSXlkOVpzY0xNc1ArSmdYWHJxRVdFYnlYcDNIT3VpaFJ2aTA4Ni0tNzUrS2cwVzdlUlpRRUZOL2RtUmdFUT09--3b51d83788265590922cd48cb2fcb9baaf82bcb2sessionKey=sessionKey=sessionKey=sessionKey="
+    ],
+    "Cookiecounter": 3,
+    "CookieIndex": 0,
+    "ProxyPassword": "",
+    "Ip": "127.0.0.1",
+    "Port": 8444,
+    "localtunnel": false,
+    "BufferSize": 1,
+    "SystemInterval": 3,
+    "rProxy": "https://claude.ai",
+    "api_rProxy": "",
+    "padtxt_placeholder": "",
+    "PromptExperimentFirst": "",
+    "PromptExperimentNext": "",
+    "PersonalityFormat": "{{char}}'s personality: {{personality}}",
+    "ScenarioFormat": "Dialogue scenario: {{scenario}}",
+    "Settings": {
+        "RenewAlways": true,
+        "RetryRegenerate": false,
+        "PromptExperiments": true,
+        "SystemExperiments": true,
+        "PreventImperson": false,
+        "AllSamples": false,
+        "NoSamples": false,
+        "StripAssistant": false,
+        "StripHuman": false,
+        "PassParams": false,
+        "ClearFlags": true,
+        "PreserveChats": false,
+        "LogMessages": true,
+        "FullColon": true,
+        "padtxt": 15000,
+        "xmlPlot": true,
+        "Superfetch": true
+    }
+}
+
+/*
+ BufferSize
+ * How many characters will be buffered before the AI types once
+ * lower = less chance of `PreventImperson` working properly
+
+ ---
+
+ SystemInterval
+ * How many messages until `SystemExperiments alternates`
+
+ ---
+
+ Other settings
+ * https://gitgud.io/ahsk/clewd/#defaults
+ * and
+ * https://gitgud.io/ahsk/clewd/-/blob/master/CHANGELOG.md
+ */
